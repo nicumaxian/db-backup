bed60b874d6444c72899b3defc610c172d360e30
Andrian Boscanean <andrian.boscanean@synkwise.com>
2021-11-02 20:12:18 +0200

    add port question

--- commands/validators.go
+++ commands/validators.go
@@ -7,6 +7,7 @@ import (
 )
 
 var nameRegex = regexp.MustCompile(`^[A-z\d\-\_]+$`)
+var portRegex = regexp.MustCompile("^[0-9]+$")
 
 func validateExistingConfigEntry() func(ans interface{}) error {
 	return func(ans interface{}) error {
@@ -52,6 +53,26 @@ func validateName() func(ans interface{}) error {
 
 		if !nameRegex.MatchString(str) {
 			return fmt.Errorf("configuration name can contain only characters, digits, hyphens or underscore")
+		}
+
+		return nil
+	}
+}
+
+func validatePort() func(ans interface{}) error {
+	return func(ans interface{}) error {
+		str, ok := ans.(string)
+		if !ok {
+			return fmt.Errorf("please provide port number")
+		}
+
+		if !portRegex.MatchString(str) {
+			return fmt.Errorf("port number should contain digits only")
+		}
+
+		port, _ := ans.(int32)
+		if port > 65536 {
+			return fmt.Errorf("port number cannot be greater than 65536")
 		}
 
 		return nil
--- configuration/model.go
+++ configuration/model.go
@@ -3,6 +3,7 @@ package configuration
 type DbConfiguration struct {
 	Driver   string `mapstructure:"driver"`
 	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
 	Username string `mapstructure:"username"`
 	Password string `mapstructure:"password"`
 	Database string `mapstructure:"database"`
