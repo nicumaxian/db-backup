33303d14c5dbaa620f627a135e196fa102e92efe
Nicu Maxian <maxiannicu@gmail.com>
2021-11-02 23:22:43 +0200

    Add spinner

--- commands/backup.go
+++ commands/backup.go
@@ -4,8 +4,11 @@ import (
 	"db-backup/configuration"
 	"db-backup/drivers"
 	"db-backup/storage"
+	"fmt"
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/urfave/cli/v2"
+	"github.com/briandowns/spinner"
+	"time"
 )
 
 func BackupCommand() *cli.Command {
@@ -31,21 +34,30 @@ func BackupCommand() *cli.Command {
 				return err
 			}
 
+
 			path, err := storage.GetNewBackupPath(name)
 			if err != nil {
 				return err
 			}
 
+			fmt.Println("Creating agent")
 			client, err := drivers.CreateDbClient(cfg.Databases[name])
 			if err != nil {
 				return err
 			}
 
+			s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+			s.Start()
+			fmt.Println("Backing up")
 			err = client.Backup(path)
+			s.Stop()
 			if err != nil {
 				return err
 			}
 
+
+			fmt.Println("Done")
+			fmt.Println(path)
 			return nil
 		},
 	}
